0b22b467ea3a648dd4095c7bcfe5b972304b8802
Ray Cheung <ray.cheung@entone.com>
2012-09-30 06:17:18 +0800

    Allow a larger patch size

--- bsdiff.c
+++ bsdiff.c
@@ -211,7 +211,7 @@ offtout(off_t x,u_char *buf)
 off_t
 bsdiff_patchsize_max(off_t newsize, off_t oldsize)
 {
-  return newsize+oldsize+1024;
+  return newsize+oldsize+PATCH_MAX_SIZE;
 }
 
 int bsdiff(u_char* oldp, off_t oldsize,
